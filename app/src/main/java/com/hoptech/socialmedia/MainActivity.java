5a75d818fa3ef07b56cbbd3bea1ca361962971a4
HOSSOU Aubin <caubinhoss@gmail.com>
2022-10-18 04:26:55 +0200

    realisation de la start page

--- app/src/main/java/com/hoptech/socialmedia/MainActivity.java
+++ app/src/main/java/com/hoptech/socialmedia/MainActivity.java
@@ -2,13 +2,19 @@ package com.hoptech.socialmedia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int TIME_OUT = 10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
