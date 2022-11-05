98a81f276a4f83ace9575048d219f81626be2b39
Amos SOSSOUKPE <fenouamossossoukpe@gmail.com>
2022-11-05 15:40:54 +0100

    realisation du fragment profil et l'activité post

--- app/src/main/java/com/hoptech/socialmedia/Activities/SignUpActivity.java
+++ app/src/main/java/com/hoptech/socialmedia/Activities/SignUpActivity.java
@@ -143,6 +143,7 @@ public class SignUpActivity extends AppCompatActivity {
         if(validateFirstname() && validateLastname() && validatePhone() && validatePassword()){
             return true;
         }else{
+
             return false;
         }
     }
